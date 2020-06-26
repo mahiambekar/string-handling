98c2ffabfbbea93576898985f20f7b8cfd77f4e6
mahiambekar <66378229+mahiambekar@users.noreply.github.com>
2020-06-26 15:48:13 +0530

    Add files via upload

---
+++ Concating Strings.java
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		String s2="hello";
+		String s3="world";
+		System.out.println(s2.concat(s3));
+		System.out.println(s2+s3);
+	
+	}
+}
