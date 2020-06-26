092a5238490333e267d84d86b793b89a2a6eb86f
mahiambekar <66378229+mahiambekar@users.noreply.github.com>
2020-06-26 15:24:25 +0530

    Add files via upload

---
+++ Creating string objects.java
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		char[]arr={'A','B','C','D'};
+		String s1= new String(arr);
+		System.out.println(s1);
+		
+		byte[]arr={65,66,67,68};
+		String s2= new String(arr1);
+		System.out.println(s2);
+		
+		String s3=new String(s2);
+		System.out.println(s3);
+		
+	
+	}
+}
