e9aa2f6bb8081af6ed50d1600fbbd9b8cd7c2f2d
mahiambekar <66378229+mahiambekar@users.noreply.github.com>
2020-06-29 10:59:55 +0530

    Add files via upload

---
+++ StartsWith_method.java
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		String s1=new String("Hello World");
+		if(s1.startsWith("He")){
+		    System.out.println("Ok");
+		}
+		else{
+		    System.out.println("Not OK");
+		}
+	}
+}
