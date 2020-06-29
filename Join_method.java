91cb502c4213e2675a992aa94e25fb3e82551fe8
mahiambekar <66378229+mahiambekar@users.noreply.github.com>
2020-06-29 11:00:42 +0530

    Add files via upload

---
+++ Join_method.java
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		String s1= String.join(",","red","green","yellow");
+		System.out.println(s1);
+		
+		s1=String.join(" ","red","green","yellow");
+		System.out.println(s1);
+		
+		s1=String.join("$","red","green","yellow");
+		System.out.println(s1);
+		
+		
+	}
+}
